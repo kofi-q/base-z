fd8716bbc9ee6a89c76cb15b9e4419715143b199
Q <264902+kofi-q@users.noreply.github.com>
2025-06-04 17:26:16 -0500

    [build] Fix ZLS "check" build step

--- build.zig
+++ build.zig
@@ -28,15 +28,12 @@ pub fn build(b: *std.Build) void {
     b.addNamedLazyPath("autodoc/main.js", b.path("src/autodoc/main.js"));
     b.addNamedLazyPath("autodoc/main.css", b.path("src/autodoc/main.css"));
 
-    const lib_check = b.addLibrary(.{
-        .name = "base-z",
-        .root_module = mod_base,
-    });
-    step_check.dependOn(&lib_check.step);
-
     const docs_install = addDocs(
         b,
-        lib_check,
+        b.addLibrary(.{
+            .name = "base-z",
+            .root_module = mod_base,
+        }),
         .{
             .dep_base = &dep_base_internal,
             .html_logo = "base-z",
@@ -48,6 +45,8 @@ pub fn build(b: *std.Build) void {
     step_docs.dependOn(&docs_install.step);
 
     const lib_test = b.addTest(.{ .root_module = mod_base });
+    step_check.dependOn(&lib_test.step);
+
     const run_test = b.addRunArtifact(lib_test);
     step_test.dependOn(&run_test.step);
 }
